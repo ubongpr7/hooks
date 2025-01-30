ed239acded0510fe12f727abf2ac29cf3dfaf6fc
UbongPr7 <ubongpr7@gmail.com>
2025-01-30 19:59:26 +0000

    modal setup and scripting

--- modal_env.py
+++ modal_env.py
@@ -1,36 +1,5 @@
 import modal
-
-image = (
-    modal.Image.debian_slim(python_version="3.10")
-    .apt_install(
-        "wget",
-        "build-essential",
-        "libssl-dev",
-        "libffi-dev",
-        "libespeak-dev",
-        "zlib1g-dev",
-        "libmupdf-dev",
-        "libfreetype6-dev",
-        "ffmpeg",
-        "espeak",
-        "imagemagick",
-        "git",
-        "postgresql",
-        "postgresql-contrib",
-        "libfreetype6",
-        "libfontconfig1",
-        "fonts-liberation"
-    )
-    .run_commands(
-        # Remove Python 3.10 build commands (already in base image)
-        "sed -i 's/<policy domain=\"path\" rights=\"none\" pattern=\"@\\*\"/<!--<policy domain=\"path\" rights=\"none\" pattern=\"@\\*\"-->/' /etc/ImageMagick-6/policy.xml || true",
-        "sed -i 's/<policy domain=\"path\" rights=\"none\" pattern=\"@\\*\"/<!--<policy domain=\"path\" rights=\"none\" pattern=\"@\\*\"-->/' /etc/ImageMagick-7/policy.xml || true"
-    )
-    .add_local_dir(".", remote_path="/app")  # Updated method
-    .pip_install("-r requirements.txt")
-    .env({"PYTHONPATH": "/app"})
-)
-
+image = modal.Image.from_registry("nas415/hooks:latest")
 app = modal.App(
     name="django-hook-processor",
     image=image
